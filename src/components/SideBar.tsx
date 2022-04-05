1b7a23219cf634e906d35891b4de70de2bef2578
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-05 13:22:07 +0200

    Using and creating SideBar code

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,22 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  genre:{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }
+  selectedGenreId:number;
+  handleClickButton:(id: number)=>void;
+}
+export function SideBar({genre, selectedGenreId, handleClickButton}:GenreResponseProps) {
+  return(
+    <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+    />
+  )
 }
