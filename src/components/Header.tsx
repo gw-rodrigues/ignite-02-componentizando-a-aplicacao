5d09bccc13e087a1112f7b7b9a21604db07ff6e3
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-05 11:26:49 +0200

    Updating Header code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,15 @@
-export function Header() {
-    // Complete aqui
+interface GenreResponseProps {
+    selectedGenre:{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+    }
+}
+
+export function Header({selectedGenre}:GenreResponseProps) {
+    return(
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+    )
 }
